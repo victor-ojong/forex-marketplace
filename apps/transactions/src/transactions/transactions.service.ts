866c7d2f6f04757781ff604e68aa6186f5882c40
Victor Ojong <victorojong78@gmail.com>
2024-04-19 23:07:32 +0100

    Feat: Add buyOrder

--- apps/transactions/src/transactions/transactions.service.ts
+++ apps/transactions/src/transactions/transactions.service.ts
@@ -14,11 +14,61 @@ export class TransactionsService {
   ) {}
 
   async buyOrder(createTransactionDto: CreateTransactionDto) {
-    // get user input and do api grpc call to rates service
-    // get API reference value
+    const [baseCurrency, currency] =
+      createTransactionDto.currencyPair.split('/');
 
-    //
-    return 'This action adds a new transaction';
+    const amount = createTransactionDto.buyPrice;
+
+    // check if user has the currency in his wallet
+
+    const wallet = await this.getwallet(createTransactionDto.walletID);
+
+    const currencyExist = wallet.filter((el) => el.currency === currency);
+
+    if (wallet.length < 1) {
+      return { status: 'fail', message: 'no wallet balance found' };
+    }
+
+    if (currencyExist.length < 1) {
+      return {
+        status: 'fail',
+        message: `you have no balance in your wallet in ${currency}`,
+      };
+    }
+
+    const exchangeValue = await this.getRatesValue(
+      baseCurrency,
+      currency,
+      amount,
+    );
+
+    if (!this.isValidBalance(exchangeValue, amount)) {
+      return {
+        status: 'fail',
+        message: `your current ballance is too low for this transaction`,
+      };
+    }
+
+    //1. subtract base value from wallet, add exchangeValue and currency as new wallet balance and save
+    const newWallet = wallet.map((el) => {
+      if (el.currency === currency) {
+        el.amount -= amount;
+      }
+      return el;
+    });
+    // creating new wallet currency
+    const addWallet = this.walletRepo.create({
+      walletID: createTransactionDto.walletID,
+      amount,
+      currency: baseCurrency,
+    });
+
+    // wallet updating
+    await this.walletRepo.save(newWallet.concat(addWallet));
+
+    //2. create a transaction document and save it
+    const newTransaction = this.transactionRepo.create(createTransactionDto);
+    return await this.transactionRepo.save(newTransaction);
   }
 
   async sellOrder(createTransactionDto: CreateTransactionDto) {
@@ -26,27 +76,29 @@ export class TransactionsService {
     // get API reference value
 
     //
-    return 'This action adds a new transaction';
-  }
-  async calculateExchangeValue(rate: number, amount: number) {
-    return rate * amount;
+    return createTransactionDto;
   }
 
   async viewHistory(walletID: string) {
     return await this.transactionRepo.find({ where: { walletID } });
-    //
   }
 
   async viewMarket() {
     //just get the market value and return
   }
 
-  async checkValidBalance() {
-    //
+  async isValidBalance(exchangeValue: number, userBalance: number) {
+    return exchangeValue <= userBalance;
   }
 
-  async getRates(rates: any, baseCurrency: string, currency: string) {
-    //
-    rates.conversion_rates[currency];
+  async getRatesValue(baseCurrency: string, currency: string, amount: number) {
+    // do a grpc call and get rates using the base currency
+    const rate = rates.conversion_rates[currency];
+
+    return rate * amount;
+  }
+
+  async getwallet(walletID: string) {
+    return await this.walletRepo.find({ where: { walletID } });
   }
 }
